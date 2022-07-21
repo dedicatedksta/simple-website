54b3aca1bc53dde601fd57962a2c3c594289e8a4
Амиш Любимов <amish_lubimov@mail.ru>
2022-07-21 17:50:09 +0300

    mobile nav

--- src/components/mainImage/MainImage.jsx
+++ src/components/mainImage/MainImage.jsx
@@ -5,12 +5,12 @@ import {BsChevronDown} from "react-icons/bs"
 const MainImage = () => {
   return <div className=" w-full relative flex justify-center items-center">
     <div className="absolute z-10 text-center text-white">
-      <h1 className="text-3xl md:text-6xl my-4">Мы – супермодели!</h1>
-      <h2 className="text-xl md:text-4xl my-4">В мир несем красоту!</h2>
-      <span className="text-base md:text-xl my-8">Модельная студия создана специально для тебя</span>
+      <h1 className="text-2xl md:text-6xl my-4">Мы – супермодели!</h1>
+      <h2 className="text-lg md:text-4xl my-4">В мир несем красоту!</h2>
+      <span className="text-sm md:text-xl my-8">Модельная студия создана специально для тебя</span>
     </div>
     <img className="   w-full brightness-[0.35]" src={mainImage} alt="" />
-    <BsChevronDown className="z-10 animate-bounce absolute bottom-0 text-4xl" color="white" />
+    <BsChevronDown className="z-10 animate-bounce absolute bottom-0 text-2xl md:text-4xl" color="white" />
   </div>;
 };
 
--- src/components/ui/Navbar.jsx
+++ src/components/ui/Navbar.jsx
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import {HiViewList} from "react-icons/hi"
+import {RiCloseFill} from "react-icons/ri"
 
 const Navbar = () => {
-  return <nav className="flex py-7 px-8 bg-black text-white font-bold justify-between text-lg items-center">
-    <ul className="flex  ">
+  const [isVisible,setIsVisible]=useState(false)
+
+  return <nav className={`transition-all ease-in-out duration-300 ${isVisible? "h-screen" :"h-16"} flex flex-col md:flex-row py-7 px-8 bg-black text-white font-bold justify-center md:leading-normal leading-9 md:justify-between text-lg items-center `}>
+    <ul className={`md:flex ${!isVisible? "hidden" :""}`}>
       <li className="px-3 cursor-pointer transition-all ease-in-out duration-300 hover:text-slate-300">О нас</li>
       <li className="px-3 cursor-pointer transition-all ease-in-out duration-300 hover:text-slate-300">Дисциплины</li>
       <li className="px-3 cursor-pointer transition-all ease-in-out duration-300 hover:text-slate-300">Педагоги</li>
       <li className="px-3 cursor-pointer transition-all ease-in-out duration-300 hover:text-slate-300">Контакты</li>
     </ul>
-    <span className=" font-normal text-2xl">+7 999 999 99 99</span>
+    <span className={`${!isVisible? "hidden" :""} pt-2 md:pt-0 md:block font-normal text-xl md:text-2xl`}>+7 999 999 99 99</span>
+
+    {
+      !isVisible?
+      <HiViewList onClick={()=>setIsVisible(!isVisible)} className={`text-2xl cursor-pointer md:hidden absolute top-5 right-6`}
+      
+      />
+      :<RiCloseFill onClick={()=>setIsVisible(!isVisible)} className={`cursor-pointer md:hidden absolute top-5 text-2xl right-6`}/>
+    }
+    
   </nav>;
 };
 
