264ad12188e78d54b9ed809e50e7817a88a1a047
Амиш Любимов <amish_lubimov@mail.ru>
2022-07-23 14:13:51 +0300

    finished

--- src/components/ui/Map.jsx
+++ src/components/ui/Map.jsx
@@ -3,9 +3,9 @@ import map from "../../assets/map/map.png"
 import {FaTelegram,FaTwitterSquare} from "react-icons/fa"
 
 const Map = () => {
-  return <div id="contacts" className="mt-40 relative">
+  return <div id="contacts" className="mt-40 relative flex flex-col-reverse md:block">
     <img src={map} className="w-screen h-[500px] object-cover"  alt="" />
-    <div className="absolute bg-white top-[15%] left-[10%] md:top-[20%] md:left-[25%] max-w-2xl p-12 ">
+    <div className="md:absolute bg-white top-[15%] left-[10%] md:left-0 lg:top-[20%] lg:left-[10%] xl:left-[15%] md:max-w-xs lg:max-w-2xl 2xl:left-[25%] p-12 ">
       <h1 className="mb-8 text-2xl font-bold">Контакты</h1>
       <div className="flex flex-col leading-6   ">
         <span>+7 999 999 99 99</span>
