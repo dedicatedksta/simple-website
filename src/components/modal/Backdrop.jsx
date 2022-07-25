1fd2fc27dd4957d1ce35228f2d008a2b03353073
Амиш Любимов <amish_lubimov@mail.ru>
2022-07-25 15:25:53 +0300

    msg

--- src/components/modal/Backdrop.jsx
+++ src/components/modal/Backdrop.jsx
@@ -5,16 +5,16 @@ import { motion,AnimatePresence } from "framer-motion";
 const Backdrop = ({children,close,visibility}) => {
   
 
-  // if (visibility){
-  //   let TopScroll = window.pageYOffset || document.documentElement.scrollTop
-  //   let LeftScroll = window.pageXOffset || document.documentElement.scrollLeft 
-  //   window.onscroll=()=>{
-  //     window.scrollTo(LeftScroll, TopScroll);
-  //     }
-  //   }
-  // else {
-  //   window.onscroll = function() {};
-  // }
+  if (visibility){
+    let TopScroll = window.pageYOffset || document.documentElement.scrollTop
+    let LeftScroll = window.pageXOffset || document.documentElement.scrollLeft 
+    window.onscroll=()=>{
+      window.scrollTo(LeftScroll, TopScroll);
+      }
+    }
+  else {
+    window.onscroll = function() {};
+  }
 
   return <AnimatePresence>
     {visibility &&
@@ -22,9 +22,9 @@ const Backdrop = ({children,close,visibility}) => {
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
-      onClick={close} className={`${visibility? "flex":"hidden"} inset-0 flex-col justify-center items-center h-screen w-screen backdrop-brightness-[0.25] fixed`}>
-        <div className="self-end px-3">
-          <RiCloseFill className="cursor-pointer self-end text-4xl text-white"/>
+      onClick={close} className={`${visibility? "flex":"hidden"} inset-0  justify-center items-center h-screen w-screen backdrop-brightness-[0.25] fixed`}>
+        <div className=" absolute top-3 right-0 md:top-3 md:right-3 ">
+          <RiCloseFill className="cursor-pointer  text-4xl text-white"/>
         </div>
         {children}
       </motion.div>)
